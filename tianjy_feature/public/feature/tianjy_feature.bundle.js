9c0504f375c406626cd44176335a90c403a2fd69
Fierflame <git@fierflame.com>
2024-01-10 15:16:17 +0800

    优化：将对评论的扩展统一到同一 APP 下

--- tianjy_feature/public/feature/tianjy_feature.bundle.js
+++ tianjy_feature/public/feature/tianjy_feature.bundle.js
@@ -1,6 +1,5 @@
 import './MultiLink';
 import './back_by_new';
-import './comment';
 
 // 批量导入中增加外部标识的支持
 import './data_import/frappe.data_import.ImportPreview';
@@ -9,7 +8,7 @@ import './data_import/frappe.data_import.ImportPreview';
 import './DateRange/frappe.ui.form.ControlDateRange.prototype.set_datepicker';
 
 // 文档附件的的显示处理
-import './from/Attachments/frappe.ui.form.Attachments';
+import './from/Attachments';
 
 // 过滤器select 包含和不包含显示label
 import './MultiSelect/frappe.ui.form.ControlMultiSelect_for_show_label.js';
